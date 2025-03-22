a8f489569c2eaa1d7669500c6f0e2270043e6d17
Aiarsien Diachkovskii <ayarsen1995@gmail.com>
2025-03-21 21:04:39 -0400

    Initial Commit

---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Aiarsien on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
